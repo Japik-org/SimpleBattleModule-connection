5747be8c73f0ea1768b5343b290b1d8d5123f3f1
pro100kryto <pro100kryto.original@gmail.com>
2021-04-30 15:33:56 -0300

    Add empty interface

---
+++ src/main/java/com/pro100kryto/server/modules/simplebattle/connection/ISimpleBattleModuleConnection.java
@@ -0,0 +1,7 @@
+package com.pro100kryto.server.modules.simplebattle.connection;
+
+import com.pro100kryto.server.module.IModuleConnection;
+
+public interface ISimpleBattleModuleConnection extends IModuleConnection {
+
+}
